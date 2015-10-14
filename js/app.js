5a9021156725e54fad0eb5538f25a99dc628a9aa
Erich Nascimento <erichnascimento@gmail.com>
2015-10-14 13:27:23 -0300

    Add suport to read args from url query string

--- js/app.js
+++ js/app.js
@@ -1,17 +1,33 @@
 "use strict";
 
-function initialize() {
-  var settings = {
-    durations: {
-      pomodoro: 25,
-      shortBreak: 5,
-      longBreak: 15
-    },
-    longBreakEach: 4
-  };
+/**
+ * Extract args from query string URL
+ */
+function getArg(argname, defaultValue) {
+  var regex = new RegExp('\\?.*?' + argname + '=([^&]*)&?');
+  var parts = location.search.match(regex);
+  if (!parts) {
+    return defaultValue;
+  }
 
+  return (parts.length == 2)
+    ? parts[1]
+    : true
+}
+
+function initialize() {
   var app = document.getElementById('pomoApp');
-  //app.setAttribute('settings', settings);
+  app.setAttribute('pomodoro-duration',
+    getArg('pm', app.getAttribute('pomodoro-duration')));
+
+  app.setAttribute('short-break-duration',
+    getArg('sb', app.getAttribute('short-break-duration')));
+  app.setAttribute('long-break-duration', getArg('lb',
+    app.getAttribute('long-break-duration')));
+  app.setAttribute('long-break-interval', getArg('lbi',
+    app.getAttribute('long-break-interval')));
+
+  app.init();
   //app.start();
   //setInterval(function(){app.next()}, 3000);
 }
